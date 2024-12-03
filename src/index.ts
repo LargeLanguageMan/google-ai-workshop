c6e332e06462ac7f135a86d01e77680c1c73f051
largelanguageman <parapet_pigment0j@icloud.com>
2024-12-03 15:36:05 +1100

    added an example of structured output

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// Make sure to include these imports:
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
+
+dotenv.config();
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const prompt = `List a few popular cookie recipes using this JSON schema:
+
+Recipe = {'recipeName': string}
+Return: Array<Recipe>`;
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
